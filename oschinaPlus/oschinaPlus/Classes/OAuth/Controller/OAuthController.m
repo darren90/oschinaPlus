8d7603268c300bdbf0fd6415f5174079483227e4
darren90 <1005052145@qq.com>
2015-11-28 13:01:37 +0800

    oath

--- oschinaPlus/oschinaPlus/Classes/OAuth/Controller/OAuthController.m
+++ oschinaPlus/oschinaPlus/Classes/OAuth/Controller/OAuthController.m
@@ -8,7 +8,7 @@
 
 #import "OAuthController.h"
 
-@interface OAuthController ()
+@interface OAuthController ()<UIWebViewDelegate>
 
 @end
 
@@ -17,21 +17,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIWebView *webView = [[UIWebView alloc]init];
+    [self.view addSubview:webView];
+    webView.frame = self.view.bounds;
+    webView.delegate = self;
+    
+    //http://www.oschina.net/action/oauth2/authorize?response_type=code&client_id=vAdtOo4xaeocczPzRBK8&redirect_uri=http://my.oschina.net/fengtengfei/blog
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.oschina.net/action/oauth2/authorize?response_type=code&client_id=%@&redirect_uri=%@",KOSCAPPID,KOSCRedirectUri]];
+    NSURLRequest *requet = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:requet];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+//    [MBProgressHUD showMessage:@"Loading"];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+//    [MBProgressHUD hideHUD];
 }
-*/
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+//    [MBProgressHUD hideHUD];
+}
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    NSString *url = request.URL.absoluteString;
+    NSRange range = [url rangeOfString:@"code="];
+    if (range.location != NSNotFound) {
+        NSLog(@"%@",url);
+        int fromIndex = (int)(range.location +range.length);
+        NSString *code = [url substringFromIndex:fromIndex];
+        
+        //利用code换取accessToken；
+        [self accessTokenWithCode:code];
+        return NO;
+    }
+    return YES;
+}
+
+
+-(void)accessTokenWithCode:(NSString *)code
+{
+//    NSString *url = @"https://api.weibo.com/oauth2/access_token";
+//    
+//    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+//    mgr.responseSerializer = [AFJSONResponseSerializer serializer];
+//    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+//    dict[@"client_id"] = KWeiboAPPKey;
+//    dict[@"client_secret"] = KWeiboAPPSecret;
+//    dict[@"grant_type"] = @"authorization_code";
+//    dict[@"code"] = code;
+//    dict[@"redirect_uri"] = KRedirectUri;
+//    
+//    [mgr POST:url parameters:dict success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+//        [MBProgressHUD hideHUD];
+//        
+//        Account *account = [Account accountWithDict:responseObject];
+//        
+//        [AccountTool saveAccount:account];
+//        
+//        [UIWindow switchRootViewVC];
+//        
+//    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
+//        TFLog(@"faile---:%@",error);
+//        [MBProgressHUD hideHUD];
+//    }];
+    
+}
+
+
 
 @end
