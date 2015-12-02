6f6fee9e0704092af9f8dda8df6f499541c7ac12
darren90 <1005052145@qq.com>
2015-12-02 22:37:21 +0800

    init bbs data

---
+++ oschinaPlus/oschinaPlus/Classes/BBS/Controller/BBSController.h
@@ -0,0 +1,13 @@
+//
+//  BBSController.h
+//  oschinaPlus
+//
+//  Created by Tengfei on 15/12/2.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBSController : UITableViewController
+
+@end
---
+++ oschinaPlus/oschinaPlus/Classes/BBS/Controller/BBSController.m
@@ -0,0 +1,127 @@
+//
+//  BBSController.m
+//  oschinaPlus
+//
+//  Created by Tengfei on 15/12/2.
+//  Copyright © 2015年 tengfei. All rights reserved.
+//
+
+#import "BBSController.h"
+
+@interface BBSController ()
+
+@end
+
+@implementation BBSController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    [self initData];
+}
+
+-(void)initData
+{
+    
+//    access_token	true	string	oauth2_token获取的access_token
+//    catalog	false	int	类别ID 1-问答 2-分享 3-IT杂烩(综合) 4-站务 100-职业生涯 0-所有	0
+//    tag	false	string	帖子相关标签
+//    pageSize	false	int	每页条数	20
+//    page/pageIndex	false	int	页数	1
+//    dataType	true	string	返回数据类型['json'|'jsonp'|'xml']	json
+
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"access_token"] = @"1fe2defc-c009-4f06-bfce-03ef5c9389a7";
+//    params[@"catalog"] = @(1);
+    params[@"page"] = @(1);
+    params[@"pageSize"] = @(20);
+    params[@"dataType"] = @"json";
+    
+    [AFNTool getWithURL:[NSString getUrlWithPort:KNews] params:params success:^(id json) {
+        NSLog(@"--j:%@",json);
+//        NewsModel *news = [NewsModel mj_objectWithKeyValues:json];
+//        [self.dataArray addObjectsFromArray:news.newslist];
+//        [self.tableView reloadData];
+    } failure:^(NSError *error) {
+        NSLog(@"--eee:%@",error);
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
